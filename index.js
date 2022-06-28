6c85b21737123588e5c7bd2d3d2e2053bd4892c4
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-06-28 15:25:25 +0700

    Thong bao realtime toan admin

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set("views","./views");
+const express = require('express');
+const { Server } = require("socket.io");
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+// const io = new Server(server);
 
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
+const io = require('socket.io')(server, {
+    //path: '/socket/laravel/socket.io',
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
-io.on("connection" ,function (socket) {
-    console.log("co ng truy cap: " + socket.id);
-    socket.on("disconnect",function () {
-        console.log(socket.id + " ngat ket noi");
+io.on("connection", function (socket) {
+    // chat message
+    socket.on('client-send-alert', (title, content) => {
+        console.log(title, content);
+
+        socket.emit('server-send-alert', {'status': 1, 'title':'Thông báo' , 'content': 'Gửi thành công !!'});  
+        socket.broadcast.emit('server-send-alert', {'status': 1, 'title':title , 'content': content});  
     });
+
 })
 app.get("/", function (req, res) {
-    res.render("trangchu");    
+    res.render("trangchu");
 });
 
 
